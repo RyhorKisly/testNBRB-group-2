a0d6df6e3a1de93b3e9a3cff7778e2d580c59ce1
Grisha <ruhorkisly@gmail.com>
2023-06-03 20:50:01 +0300

    добавил дао

---
+++ src/main/java/dao/DaoCurrency.java
@@ -0,0 +1,40 @@
+package dao;
+
+import core.Currency;
+import dao.api.IDaoCurrency;
+import dao.ds.api.IDataSourceWrapper;
+import dao.ds.fabric.DataSourceSingleton;
+
+import java.util.List;
+
+public class DaoCurrency implements IDaoCurrency {
+    IDataSourceWrapper iDataSourceWrapper;
+    public DaoCurrency(IDataSourceWrapper iDataSourceWrapper) {
+        this.iDataSourceWrapper = iDataSourceWrapper;
+    }
+
+    @Override
+    public long getID(String typeCurrency) {
+        return 0;
+    }
+
+    @Override
+    public void saveCurrency(List<Currency> currencies) {
+
+    }
+
+    @Override
+    public boolean exist(String typeCurrency) {
+        return false;
+    }
+
+    @Override
+    public List<Currency> getCurrency() {
+        return null;
+    }
+
+    @Override
+    public void remove(long id) {
+
+    }
+}
