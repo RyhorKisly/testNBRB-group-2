7d12bd976d51a796238dcf51a8a031c7cc4e931a
Pasha <Horvard777@mail.ru>
2023-06-09 00:08:45 +0300

    long - double

--- src/main/java/dao/DaoStatisticCurrency.java
+++ src/main/java/dao/DaoStatisticCurrency.java
@@ -75,7 +75,7 @@ public class DaoStatisticCurrency implements IDaoStatisticCurrency {
                 String abbreviation = rs.getString("abbreviation");
                 long scale = rs.getLong("scale");
                 String name = rs.getString("name");
-                long officialRate = rs.getLong("official_rate");
+                double officialRate = rs.getDouble("official_rate");
                 statisticCurrencies.add(new StatisticCurrency(
                         id,date, abbreviation,scale,name, officialRate));
             }
@@ -102,7 +102,7 @@ public class DaoStatisticCurrency implements IDaoStatisticCurrency {
                 String abbreviation = rs.getString("abbreviation");
                 long scale = rs.getLong("scale");
                 String name = rs.getString("name");
-                long officialRate = rs.getLong("official_rate");
+                double officialRate = rs.getDouble("official_rate");
                 statisticCurrencies.add(new StatisticCurrency(
                         id,date, abbreviation,scale,name, officialRate));
             }
